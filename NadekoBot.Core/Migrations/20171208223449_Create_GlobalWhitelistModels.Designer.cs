2a1b538d84bb8dd995ff860f36656ba5d8751d64
Flying Katsu <Warped2713@users.noreply.github.com>
2017-12-08 18:52:38 -0500

    added new database models for globalwhitelist #4

--- NadekoBot.Core/Migrations/20171208223449_Create_GlobalWhitelistModels.Designer.cs
+++ NadekoBot.Core/Migrations/20171208223449_Create_GlobalWhitelistModels.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace NadekoBot.Migrations
 {
     [DbContext(typeof(NadekoContext))]
-    partial class NadekoSqliteContextModelSnapshot : ModelSnapshot
+    [Migration("20171208223449_Create_GlobalWhitelistModels")]
+    partial class Create_GlobalWhitelistModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
